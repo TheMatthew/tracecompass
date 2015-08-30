2bf9bcd7c663eacf2c72d38d38cbe57ac9174515
Matthew Khouzam <matthew.khouzam@ericsson.com>
2015-08-29 21:37:37 -0400

    fix bo
    Change-Id: I93d84cba426f578806934d50d76419f0d49e3a97
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- ctf/org.eclipse.tracecompass.ctf.core/src/org/eclipse/tracecompass/internal/ctf/core/event/metadata/ByteOrderParser.java
+++ ctf/org.eclipse.tracecompass.ctf.core/src/org/eclipse/tracecompass/internal/ctf/core/event/metadata/ByteOrderParser.java
@@ -11,6 +11,7 @@ package org.eclipse.tracecompass.internal.ctf.core.event.metadata;
 
 import static org.eclipse.tracecompass.internal.ctf.core.event.metadata.TsdlUtils.concatenateUnaryStrings;
 import static org.eclipse.tracecompass.internal.ctf.core.event.metadata.TsdlUtils.isUnaryString;
+import static org.eclipse.tracecompass.common.core.NonNullUtils.checkNotNull;
 
 import java.nio.ByteOrder;
 
@@ -19,9 +20,29 @@ import org.eclipse.tracecompass.ctf.core.trace.CTFTrace;
 import org.eclipse.tracecompass.internal.ctf.core.event.metadata.exceptions.ParseException;
 
 /**
- * TSDL byte order parser
+ * By default, byte order of a basic type is the byte order described in the
+ * trace description. It can be overridden by specifying a byte_order attribute
+ * for a basic type. Typical use-case is to specify the network byte order (big
+ * endian: be) to save data captured from the network into the trace without
+ * conversion.
+ *
+ * TSDL metadata representation:
+ *
+ * <pre>
+ * /* network and be are aliases * /
+ * byte_order= /* native OR network OR be OR le * /;
+ * </pre>
+ *
+ * The native keyword selects the byte order described in the trace description. The
+ * network byte order is an alias for big endian.
+ *
+ * Even though the trace description section is not per se a type, for sake of
+ * clarity, it should be noted that native and network byte orders are only
+ * allowed within type declaration. The byte_order specified in the trace
+ * description section only accepts be or le values.
  *
  * @author Matthew Khouzam
+ * @author Efficios - Javadoc preamble
  *
  */
 public final class ByteOrderParser implements ICommonTreeParser {
@@ -49,12 +70,13 @@ public final class ByteOrderParser implements ICommonTreeParser {
             String strval = concatenateUnaryStrings(byteOrderTree.getChildren());
 
             if (strval.equals(MetadataStrings.LE)) {
-                return ByteOrder.LITTLE_ENDIAN;
+                return checkNotNull(ByteOrder.LITTLE_ENDIAN);
             } else if (strval.equals(MetadataStrings.BE)
                     || strval.equals(MetadataStrings.NETWORK)) {
-                return ByteOrder.BIG_ENDIAN;
+                return checkNotNull(ByteOrder.BIG_ENDIAN);
             } else if (strval.equals(MetadataStrings.NATIVE)) {
-                return trace.getByteOrder();
+                ByteOrder byteOrder = trace.getByteOrder();
+                return (byteOrder == null) ? checkNotNull(ByteOrder.nativeOrder()) : byteOrder;
             } else {
                 throw new ParseException(INVALID_VALUE_FOR_BYTE_ORDER);
             }
